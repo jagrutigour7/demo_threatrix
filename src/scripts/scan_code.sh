f115b8e7ab74a61692a92e10cfe6494285887f7d
jagrutiogour7 <jagrutigour1799@gmail.com>
2024-03-14 15:09:31 +0530

    Updated yml files

---
+++ src/scripts/scan_code.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar /tmp/threatagent.jar --oid=856cb634-419b-49a9-80aa-a9b5e287eb37 --eid=6c7fcedb-7172-45bd-b14f-55e68e614f90 --api-key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnaXRodWItVV9rZ0RPQmUybWN3IiwiZXhwIjoxNzI1OTcxODk2LCJpYXQiOjE3MTAzMzM0OTZ9.CK5EBfbPDw2ZOnZon7St4c5ucXdX03qCkXpZ3OhdaAk ./
