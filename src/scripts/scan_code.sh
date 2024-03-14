f2ab6915f06c7d71dd2afcf52df7d39a1424af11
jagrutiogour7 <jagrutigour1799@gmail.com>
2024-03-14 17:04:51 +0530

    Updated yml files

--- src/scripts/scan_code.sh
+++ src/scripts/scan_code.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# curl -L https://github.com/threatrix/threat-agent-dist/releases/download/latest/threat-agent.jar > /tmp/threatagent.jar
+curl -L https://github.com/threatrix/threat-agent-dist/releases/download/latest/threat-agent.jar > /tmp/threatagent.jar
 ls /tmp
 whoami
 curl icanhazip.com
