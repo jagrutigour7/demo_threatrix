b5ed9206de363036378fe6dd6ce7fe4d02aad41f
jagrutiogour7 <jagrutigour1799@gmail.com>
2024-03-14 15:22:37 +0530

    Updated yml files

--- src/scripts/install_threatagent.sh
+++ src/scripts/install_threatagent.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
 curl -L https://github.com/threatrix/threat-agent-dist/releases/download/latest/threat-agent.jar > /tmp/threatagent.jar
+whoami
+curl icanhazip.com
+ls /tmp
--- src/scripts/scan_code.sh
+++ src/scripts/scan_code.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 # curl -L https://github.com/threatrix/threat-agent-dist/releases/download/latest/threat-agent.jar > /tmp/threatagent.jar
 ls /tmp
+whoami
+curl icanhazip.com
 java -jar /tmp/threatagent.jar --oid=856cb634-419b-49a9-80aa-a9b5e287eb37 --eid=6c7fcedb-7172-45bd-b14f-55e68e614f90 --api-key=eyJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJnaXRodWItVV9rZ0RPQmUybWN3IiwiZXhwIjoxNzI1OTcxODk2LCJpYXQiOjE3MTAzMzM0OTZ9.CK5EBfbPDw2ZOnZon7St4c5ucXdX03qCkXpZ3OhdaAk ./
